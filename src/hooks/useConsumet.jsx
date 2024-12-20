c8f09c8f360aee1880685a23a53b1109a6bb990c
shubham-king <84066395+shubham-king@users.noreply.github.com>
2024-12-20 23:42:13 +0530

    #api

--- src/hooks/useConsumet.jsx
+++ src/hooks/useConsumet.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useQuery } from "react-query";
 import { servers } from "../api/gogoanime_servers";
 function handleConsumetResponse(endpoint, parameter) {
-  const BASE_URL = `https://consumet.manjotbenipal.xyz/anime/gogoanime`;
+  const BASE_URL = `https://gogo-anime-api-psi.vercel.app/`;
   const results = useQuery(`${endpoint}${parameter}`, async () => {
     if (parameter) {
       return await axios
