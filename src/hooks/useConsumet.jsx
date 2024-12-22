1cb921ad53b9eec178b461aec30d0965b911a7ed
shubham-king <84066395+shubham-king@users.noreply.github.com>
2024-12-22 12:36:02 +0530

    Update useConsumet.jsx

--- src/hooks/useConsumet.jsx
+++ src/hooks/useConsumet.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useQuery } from "react-query";
 import { servers } from "../api/gogoanime_servers";
 function handleConsumetResponse(endpoint, parameter) {
-  const BASE_URL = `https://consumet-api-vab8.onrender.com/anime/9anime`;
+  const BASE_URL = `https://consumet-api-vab8.onrender.com/anime/anify`;
   const results = useQuery(`${endpoint}${parameter}`, async () => {
     if (parameter) {
       return await axios
