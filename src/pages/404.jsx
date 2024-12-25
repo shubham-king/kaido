5c89481ad02ab808cb0202cb98f4209328889630
shubham-king <84066395+shubham-king@users.noreply.github.com>
2024-12-25 10:21:27 +0530

    Update 404.jsx

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,9 +1,13 @@
-export default function Custom404() {
-    return (
-      <div>
-        <h1>404 - Page Not Found</h1>
-        <p>The page you are looking for does not exist.</p>
-      </div>
-    );
-  }
-  
+// pages/404.jsx
+import React from 'react';
+
+const Custom404 = () => {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>404 - Page Not Found</h1>
+      <p>The page you are looking for doesn't exist.</p>
+    </div>
+  );
+};
+
+export default Custom404;
